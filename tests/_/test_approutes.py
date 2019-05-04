04f1621eed0aa531d5c6aa2ccf5ccd2d44bbfefe
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2019-05-04 20:57:48 +0200

    Add test to detect url routes that clash with the symbols

---
+++ tests/_/test_approutes.py
@@ -0,0 +1,9 @@
+from shorter.start.environment import SYM_POOL
+
+
+def test_forbidden_routes(app):
+    for route in app.url_map.iter_rules():
+        rule = route.rule.lstrip('/')
+        if rule and '/<' not in rule:
+            if all(cha in SYM_POOL for cha in rule):
+                assert False, '/{} clashes with symbols!'.format(rule)
